4e7a9da17e3a3279a7fb2cb55c9f54b1e8a24170
Your Name <Your@email>
2024-11-07 17:19:16 -0500

    Ajuest estilo buscar

--- controller/buscar.php
+++ controller/buscar.php
@@ -1,9 +1,12 @@
-<?php include "../model/conexion.php";
+<?php
+include "../model/conexion.php";
+$bandera = false;  // Inicializamos la bandera como false
+
 if (isset($_GET['query'])) {
     $searchQuery = htmlspecialchars($_GET['query']); // Obtener el término de búsqueda
 
     // Preparar la consulta SQL para buscar en nombre o descripción de los productos
-    $sql = "SELECT p.idpro, p.nompro, p.valorunitario, p.pordescu, p.valorunitario - (p.valorunitario * (p.pordescu / 100)) AS valor_con_descuento, img.imgpro FROM producto AS p LEFT JOIN (SELECT i.idpro, i.imgpro FROM imagen AS i WHERE i.idpro IS NOT NULL ORDER BY i.ordimg ASC LIMIT 1) AS img ON p.idpro = img.idpro WHERE p.nompro LIKE :query OR p.descripcion LIKE :query;";
+    $sql = "SELECT p.idpro, p.nompro, p.valorunitario, p.pordescu, p.valorunitario - (p.valorunitario * (p.pordescu / 100)) AS valor_con_descuento, img.imgpro FROM producto AS p LEFT JOIN (SELECT i.idpro, i.imgpro FROM imagen AS i WHERE i.idpro IS NOT NULL ORDER BY i.ordimg ASC LIMIT 1) AS img ON p.idpro = img.idpro WHERE p.nompro LIKE :query OR p.descripcion LIKE :query";
 
     try {
         $model = new Conexion();
@@ -17,11 +20,12 @@ if (isset($_GET['query'])) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if ($results) {
             // Mostrar productos encontrados
+            $bandera = true; // Establecer bandera a true si hay resultados
             foreach ($results as $product) {
                 echo "<div class='product row'>";
                 echo "<h3>" . htmlspecialchars($product['nompro']) . "</h3>";
                 echo "<div class='col-5 bx-sh-img'>";
-                echo "<img src='".$product['imgpro']."' alt='".$product['nompro']."'>";
+                echo "<img src='" . $product['imgpro'] . "' alt='" . $product['nompro'] . "'>";
                 echo "</div>";
                 echo "<div class='col'>";
 
@@ -39,8 +43,6 @@ if (isset($_GET['query'])) {
 
                 echo "</div>";
                 echo "</div>";
-
-
             }
         } else {
             echo "<p id='res-nf'>No se encontraron productos que coincidan con '$searchQuery'.</p>";
